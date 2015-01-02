13290670e224c6730c9836a90eb0c0b872c4c871
AVRoosin <avroosin@mail.ru>
2015-01-02 18:56:14 +0500

    Добавлена минификация html-файла
    с requirejs  пока не разобрался

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,47 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        clean: ["build"]
+        clean: ["build"],
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            'data/*',
+                            'index.html'
+                        ],
+                        dest: 'build/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'build/index.html': 'build/index.html'
+                }
+            }
+        }
+//        requirejs: {
+//            compile: {
+//                options: {
+//                    baseUrl: ".",
+//                    removeCombined: true,
+//
+//                }
+//            }
+//        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+//    grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-    grunt.registerTask('default', ['clean']);
+    grunt.registerTask('default', ['clean', 'copy', 'htmlmin']);
 };
